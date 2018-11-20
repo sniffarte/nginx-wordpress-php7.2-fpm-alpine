4467eddf38554bc1843e44a75547d6097f62d4f2
sniffarte <sniffarte@users.noreply.github.com>
2018-11-20 20:16:05 -0300

    fix Chwon

--- scripts/start.sh
+++ scripts/start.sh
@@ -61,20 +61,6 @@ fi
 
 
 
-if [ ! -z "$PUID" ]; then
-  if [ -z "$PGID" ]; then
-    PGID=${PUID}
-  fi
-  deluser nginx
-  addgroup -g ${PGID} nginx
-  adduser -D -S -h /var/cache/nginx -s /sbin/nologin -G nginx -u ${PUID} nginx
-else
-  if [ -z "$SKIP_CHOWN" ]; then
-    chown -Rf nginx.nginx /var/www/html
-  fi
-fi
-
-
 file_env() {
 	local var="$1"
 	local fileVar="${var}_FILE"
@@ -322,6 +308,19 @@ for e in "${envs[@]}"; do
   unset "$e"
 done
 
+
+if [ ! -z "$PUID" ]; then
+  if [ -z "$PGID" ]; then
+    PGID=${PUID}
+  fi
+  deluser nginx
+  addgroup -g ${PGID} nginx
+  adduser -D -S -h /var/cache/nginx -s /sbin/nologin -G nginx -u ${PUID} nginx
+else
+  if [ -z "$SKIP_CHOWN" ]; then
+    chown -Rf nginx.nginx /var/www/html
+  fi
+fi
 
 # Start supervisord and services
 exec /usr/bin/supervisord -n -c /etc/supervisord.conf
